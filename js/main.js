e8ed27c8bbcdf1823a7726653ebd7ce57df7d64b
Rowesh <302515056@student.rocmondriaan.nl>
2024-06-11 11:55:48 +0200

    Commentaar toegevoegd

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//JavaScript voor header | Achtergrondkleur wordt donkerder
 const changeCss = () => {
     const navElement = document.querySelector("nav");
     if (window.scrollY > 20) {
@@ -8,12 +9,13 @@ const changeCss = () => {
   };
   window.addEventListener("scroll", changeCss);
 
-// Getting the play buttons from the cards
+// Alle variabelen ophalen van de projectbuttons
 const playHigherLower = document.querySelector(".higher-lower");
 const playEscapeRoom = document.querySelector(".escape-room");
 const playBuildAGame = document.querySelector(".build-a-game");
 const viewRacingWebsite = document.querySelector(".racing-website");
 
+//AddEventListeners voor elk project die de gebruiker redirect naar het desbetreffende project
 playHigherLower.addEventListener("click", function () {
   window.open("Higher Lower/index.html", "_blank");
 });
@@ -30,6 +32,7 @@ viewRacingWebsite.addEventListener("click", function () {
   window.open("Racing-website/index.html", "_blank");
 });
 
+//JavaScript waardoor de gebruiker die op de knop drukt met ID 'Scroll-down' 750px naar beneden wordt genavigeerd.
 document.addEventListener('DOMContentLoaded', () => {
   const scrollButton = document.querySelector('#scroll-down');
   scrollButton.addEventListener('click', () => {
