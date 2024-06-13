af83bba5d541f8f33fc7fbad1f41ca8436358d49
Rowesh <302515056@student.rocmondriaan.nl>
2024-06-13 09:22:48 +0200

    Webcatalogus gelinkt

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 //JavaScript voor header | Achtergrondkleur wordt donkerder
 const changeCss = () => {
-    const navElement = document.querySelector("nav");
-    if (window.scrollY > 20) {
-      navElement.style.backgroundColor = "rgb(250, 250, 250)";
-    } else {
-      navElement.style.backgroundColor = ""; 
-    }
-  };
-  window.addEventListener("scroll", changeCss);
+  const navElement = document.querySelector("nav");
+  if (window.scrollY > 20) {
+    navElement.style.backgroundColor = "rgb(250, 250, 250)";
+  } else {
+    navElement.style.backgroundColor = "";
+  }
+};
+window.addEventListener("scroll", changeCss);
 
 // Alle variabelen ophalen van de projectbuttons
 const playHigherLower = document.querySelector(".higher-lower");
 const playEscapeRoom = document.querySelector(".escape-room");
 const playBuildAGame = document.querySelector(".build-a-game");
 const viewRacingWebsite = document.querySelector(".racing-website");
+const viewWebcatalogus = document.querySelector(".webcatalogus");
 
 //AddEventListeners voor elk project die de gebruiker redirect naar het desbetreffende project
 playHigherLower.addEventListener("click", function () {
@@ -32,12 +33,14 @@ viewRacingWebsite.addEventListener("click", function () {
   window.open("Racing-website/index.html", "_blank");
 });
 
-//JavaScript waardoor de gebruiker die op de knop drukt met ID 'Scroll-down' 750px naar beneden wordt genavigeerd.
-document.addEventListener('DOMContentLoaded', () => {
-  const scrollButton = document.querySelector('#scroll-down');
-  scrollButton.addEventListener('click', () => {
-      window.scrollBy({ top: 750, left: 0});
-  });
+viewWebcatalogus.addEventListener("click", function () {
+  window.open("Webcatalogus/index.html", "_blank");
 });
 
-  
+//JavaScript waardoor de gebruiker die op de knop drukt met ID 'Scroll-down' 750px naar beneden wordt genavigeerd.
+document.addEventListener("DOMContentLoaded", () => {
+  const scrollButton = document.querySelector("#scroll-down");
+  scrollButton.addEventListener("click", () => {
+    window.scrollBy({ top: 750, left: 0 });
+  });
+});
